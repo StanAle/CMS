30922ff20a2d22004344c70e9e94d9681728918c
AzureAD\Alexandra-GeorgianaS <alexandra.stan@principal33.com>
2021-04-26 10:44:15 +0300

    Add function to add a name to the search list

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ function addInTable() {
   var sex = document.getElementById("sex").value;
   var birthday = document.getElementById("birthday").value;
   var tableLength = document.getElementById("table").rows.length;
-  var row = table.insertRow(1);
+  var row = table.insertRow(tableLength);
   var cellFname = row.insertCell(0);
   var cellLname = row.insertCell(1);
   var cellEmail = row.insertCell(2);
   var cellSex = row.insertCell(3);
   var cellBirthday = row.insertCell(4);
+  addToList(fname + " " + lname);
   cellFname.innerHTML = fname;
   cellLname.innerHTML = lname;
   cellEmail.innerHTML = email;
   cellSex.innerHTML = sex;
   cellBirthday.innerHTML = birthday;
+}
+function addNameToList(name) {
+  var ul = document.getElementById("list");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(name));
+  ul.appendChild(li);
 }
